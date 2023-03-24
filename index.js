b383b1e5ba152fe4ae460cc7f25f90880530b50c
Patrick Fässler <patrick-faessler@protonmail.com>
2023-03-24 16:07:35 +0100

    port cahnge

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
-const port = 8080;
+
 const filename = __dirname + "/article.json";
 
 //Middleware
 app.use(express.json()); //for parsing application/json
 app.use(cors()); //for configuring Cross-Origin Resource Sharing (CORS)
+const port = process.env.PORT || 3333;
 function log(req, res, next) {
     console.log(req.method + " Request at" + req.url);
     next();
